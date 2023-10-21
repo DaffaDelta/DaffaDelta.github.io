0061c0b029e70961937bee5c17b8d63b24ada585
DaffaDelta <141333001+DaffaDelta@users.noreply.github.com>
2023-10-21 11:45:30 +0700

    Add files via upload

--- js/opener.js
+++ js/opener.js
@@ -1,3 +1,21 @@
+function buttonOpener() {
+    closeOpener();
+    playPause();
+}
+
 function closeOpener() {
     document.getElementById("myOpener").style.display = "none";
+}
+
+var aud = document.getElementById("ASong").children[0];
+var isPlaying = False;
+aud.pause();
+
+function playPause() {
+    if (isPlaying) {
+        aud.pause();
+    } else {
+        aud.play();
+    }
+    isPlaying = !isPlaying;
 }
